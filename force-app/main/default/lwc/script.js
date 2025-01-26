30beaa9161ce2c31a282610e1fed55fb55024989
MuraliKrishna1995 <murali.krish2013@gmail.com>
2025-01-26 15:11:55 +0530

    learning commit sunday 26 jan

--- force-app/main/default/lwc/script.js
+++ force-app/main/default/lwc/script.js
@@ -19,52 +19,52 @@ scopeTesting();
 
 
 // 1. Array 
- var arr=['a','b','c'];
- console.log(arr);
+var arr=['a','b','c'];
+console.log(arr);
 
 
- //2. object 
- var obj={
-    "name": "Murali",
-    "age":29,
-    "Technilogy job": "Salesforce developer"
- }
- console.log(obj.job)//this gives us undefines as we have space in the property name 
- console.log(obj["Technilogy job"]);// gives us the Salesforce developer
+//2. object 
+var obj={
+"name": "Murali",
+"age":29,
+"Technilogy job": "Salesforce developer"
+}
+console.log(obj.job)//this gives us undefines as we have space in the property name 
+console.log(obj["Technilogy job"]);// gives us the Salesforce developer
 
 
- // Spread operator is used to 
- //1. breaks down an array into individual elements 
+// Spread operator is used to 
+//1. breaks down an array into individual elements 
 
- let greetings = "Hi Murali ";
- let charList = [...greetings];
- console.log(charList);
+let greetings = "Hi Murali ";
+let charList = [...greetings];
+console.log(charList);
 
- //2. Combining an Array
- let arr1=["Astro","Sassy"];
- let arr2=["pod1","Enhancement"];
- let arr3=[...arr1,...arr2];
- console.log(arr3);
- // 3. adding values to array
- let arr4 = ["Miiki"];
- let arr5 =["Nikki",...arr4];
- console.log(arr5);
- //4. combining an object 
- let obj1=[
-     {
-         "name": "Murali",
-         "age":29,
-         "job": "Salesforce developer"
-     }];
+//2. Combining an Array
+let arr1=["Astro","Sassy"];
+let arr2=["pod1","Enhancement"];
+let arr3=[...arr1,...arr2];
+console.log(arr3);
+// 3. adding values to array
+let arr4 = ["Miiki"];
+let arr5 =["Nikki",...arr4];
+console.log(arr5);
+//4. combining an object 
+let obj1=[
+    {
+        "name": "Murali",
+        "age":29,
+        "job": "Salesforce developer"
+    }];
 
-     let obj2 = [
-         {
-             "name": "Nikki",
-             "age":25,
-             "job": "LIMS Developer"}];
+    let obj2 = [
+        {
+            "name": "Nikki",
+            "age":25,
+            "job": "LIMS Developer"}];
 
-             let obj3=[...obj1,...obj2];
-             console.log(obj3);
+            let obj3=[...obj1,...obj2];
+            console.log(obj3);
 
 // 5. Shallow Copy 
 // if we use traditional way of copying an array i.e .. 
@@ -79,3 +79,40 @@ var arr10 =[...arr6];
 arr10.push("Nikhita");
 console.log('print via spread operator '+ arr10);
 // Shallow copy is not reflected in the arr 6 
+
+
+/**********Destructuring***********************/
+// Destructing is a special syntax of JS that allows us to assign values from arrays and objects to variables
+let arrcomp=["Amazon","Walmart","Flipkart","Snapdeal"];
+let [first,second,third,fourth]=arrcomp;
+console.log("company 1 " +first);
+console.log("Company 2 " +second);
+/* Destructuring of object */
+let objcomp={
+"company":"Amazon",
+"price":200,
+"rating":5
+}
+let  {company,price,rating}=objcomp;
+console.log("Company array from obj "+ company);
+console.log("price array from obj "+price);
+console.log("rating array from obj "+rating);
+    /********string interopolation *************/
+    // String Interpolation is used to the process of inserting strings or values into an existing string 
+    //for various purposes
+var empname = "Murali";
+var salary = 10000;
+var bonus = 2000;
+var message = (`Hello  your salary is ${salary} and bonus is ${bonus} total ctc is ${salary +bonus}`);
+
+console.log("Whole message " +message);
+
+/* String Methods in JS */
+//Slice method extracts a section of a string and returns a new string
+var str1 = "Hello World";
+    var str2 = str1.slice(0,5);
+     console.log("Slice method example "+ str2);
+     //trim method removes the whitespaces from both sides of the string
+     var str3 = "   Hello World  ";     
+     var str4 = str3.trim();
+     console.log("trim method example " +str4);
