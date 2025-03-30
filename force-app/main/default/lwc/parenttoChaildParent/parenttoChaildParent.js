29c5779f0e3ec402ddb08171528fa756c985649f
MuraliKrishna1995 <murali.krish2013@gmail.com>
2025-03-30 15:48:06 +0530

    changes to data cmp

--- force-app/main/default/lwc/parenttoChaildParent/parenttoChaildParent.js
+++ force-app/main/default/lwc/parenttoChaildParent/parenttoChaildParent.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+/**
+ * @description       : 
+ * @author            : Murali
+ * @group             : 
+ * @last modified on  : 03-30-2025
+ * @last modified by  : Murali
+**/
+import { LightningElement,api } from 'lwc';
 
-export default class ParenttoChaildParent extends LightningElement {}
+export default class ParenttoChaildParent extends LightningElement {
+    @api greetings ;
+}
